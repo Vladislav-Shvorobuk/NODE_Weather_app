efcf1c8c1712e9cbdd5846d8a5d8cc548b8241e9
Vladislav <vladislav.shvorobuk@gmail.com>
2018-10-12 15:24:08 +0300

    test queries, add some libraries

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 "use strict"
 const express = require("express");
+const request = require('request');
+const cheerio = require('cheerio');
+let $;
+const jquery = require('jquery');
+
+
+
 const app = express();
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({extended:true});
-app.set("view engine", "ejs");
 
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/'));
 
 
@@ -13,18 +20,47 @@ app.use(express.static(__dirname + '/'));
 let city = "Lviv";
 let apiKey = "&appid=c5807c2d3469d3aeec1bc8745a7f540a";
 let url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+let cityWeather = {};
 
+request(url, function(error, res, data){
+  if (!error && res.statusCode == 200) {
+    let obj = JSON.parse(data);
+    // console.log(obj);
+    console.log(typeof obj.wind.speed);
+    cityWeather = {
+      wind:{
+        speed : obj.wind.speed,
+        deg : obj.wind.deg 
+      },
+      name : obj.name
+    }
+    
 
-app.get("/", function(req, res ){
-
-  res.render(__dirname + "/src/views/index.ejs", {id: 1000} );
-  console.log(__dirname + "/src/views/index.ejs");
+  }
 
 
 });
 
 
+app.get("/", function(req, res ){
 
+  res.render(__dirname + "/src/views/index.ejs", {
+    cityWeather: cityWeather
+  
+  })
+      
+});
+
+
+  
+// request('http://localhost:3001/', function (error, response, html) {
+//   if (!error && response.statusCode == 200) {
+//     var $ = cheerio.load(html);
+//     console.log(" WORK - " + $ + "  !!!!!!!!!");
+//     };
+// });
+
+  
 
 
 app.listen(3001, () => console.log("SERVER IS WORKING"));
