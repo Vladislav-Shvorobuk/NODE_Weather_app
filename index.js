7327ce5b266c29381c6882989bbe867c6f33ee88
Vladislav <vladislav.shvorobuk@gmail.com>
2018-10-14 03:02:04 +0300

    add    app.delete

--- index.js
+++ index.js
@@ -2,25 +2,16 @@
 const express = require("express");
 const request = require('request');
 const cheerio = require('cheerio');
-let $;
-const jquery = require('jquery');
-const bootstrap = require('jquery');
-
-
-
-const app = express();
 const bodyParser = require("body-parser");
 const urlencodedParser = bodyParser.urlencoded({extended:true});
+const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/'));
 
 
-
-
-let city = "Lviv";
-let apiKey = "&appid=c5807c2d3469d3aeec1bc8745a7f540a";
-let url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey + "&units=metric";
+const apiKey = "&appid=c5807c2d3469d3aeec1bc8745a7f540a";
+let url;
 let cities = [];
 let cityWeather = [];
 
@@ -29,18 +20,21 @@ app.get("/", function(req, res ){
 
   res.render(__dirname + "/src/views/index.ejs", {
     cities: cities
-  })
-      
+  }) 
 });
 
 app.post("/view", urlencodedParser, function(req, res){
     if(!req.body) return res.sendStatus(400);
-    console.log(req.body);
+   
     url = "http://api.openweathermap.org/data/2.5/weather?q=" + req.body.city + apiKey + "&units=metric";
 
     let promise = new Promise((resolve, reject) => {
 
     request(url, function(error, res, data){
+      if (!error && res.statusCode == 404) {
+        resolve(res.statusCode);
+        console.log(res.statusCode + " Please, check city name.");
+      }
       if (!error && res.statusCode == 200) {
         let obj = JSON.parse(data);
     
@@ -57,26 +51,30 @@ app.post("/view", urlencodedParser, function(req, res){
       }
     });
   });
-
-  promise
-  .then(
-    result => {
-      res.redirect("/");
-
-    }
-  
-  );
-
- 
-    
+     promise
+      .then(
+         result => {
+         res.redirect("/");
+        }
+     ); 
 })
 
+app.delete("/delete/:index", function(req, res){
 
+  let promise = new Promise((resolve, reject) => {
+    let index = req.params.index;
+    cities.splice(index, 1);
+    resolve();
+  });
+  promise    
+   .then(
+      result => {
+        res.send("Success");
+      }
+  );
+ 
+});
 
-
-
-
-  
 app.listen(3001, () => console.log("SERVER IS WORKING"));
 
 
--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,22 @@
 
 $(function(){
     $('.container').css('min-height', $(window).height());
-})
+
+
+    $(".delete").on("click", function(event){
+            let index = $(event.target ).attr("id");
+
+            $.ajax({
+                type: "DELETE",
+                url: "/delete/" + index,
+                success: function(res){
+                    window.location.href="/";
+                }, error: function(err) {
+                    console.log(err);
+                }
+            });
+    });
+});
  
 
 
